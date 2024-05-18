3163f4ffef31185a5cdd95e7232e603071d9596a
fdiaz20 <143426144+fdiaz20@users.noreply.github.com>
2024-05-18 11:24:27 -0400

    Add files via upload

---
+++ bot_mail.py
@@ -0,0 +1,18 @@
+from email.message import EmailMessage
+import smtplib
+
+link=""
+remitente="bot.ugma@gmail.com"
+def send_email(nombre, destino):
+    msg="Hola "+nombre+", ingresa a este link para responder la encuesta sobre tu profesor: "+link;
+    email=EmailMessage()
+    email["From"]=remitente
+    email["To"]=destino
+    email["Subject"]="Encuesta de profesores"
+    email.set_content(msg)
+    
+    smtp= smtplib.SMTP_SSL('smtp.gmail.com')
+    smtp.login(remitente, "contraseña")
+    smtp.sendmail(remitente,destino,email.as_string())
+    smtp.quit()
+    
